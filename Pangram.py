0465d671728a73172c9ffdb37fd77a8e3e5db5fe
QA <QA>
2016-08-23 21:30:43 +0300

    added full alphabet

--- Pangram.py
+++ Pangram.py
@@ -3,6 +3,6 @@ from string import ascii_lowercase as az
 def check(text):
     return set(text.lower()).issuperset(set(az))
 	
-print (check('abc')) == false
-print (check('adcdeifghjklmnopqrstuvwxyz')) == true
+print (check('abc')) == false)
+print (check('adcdeifghjklmnopqrstuvwxyz')) == true)
 
