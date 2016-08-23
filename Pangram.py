0061403653367e628f2614a39308a239d577c53d
QA <QA>
2016-08-23 22:05:20 +0300

    Fix

--- Pangram.py
+++ Pangram.py
@@ -3,4 +3,8 @@ from string import ascii_lowercase as az
 
 def check(text):
     return set(text.lower()).issuperset(set(az))
-print (check('abc'))
+	
+print (check('abc') == False)
+print (check('adcdefghijklmnopqrstuvwxyz') == True)
+print (check('ABCDEFGHIJKLMNOPQRSTUVWXYZ') == True)
+print (check('Quick brown fox jumps over the lazy dog') == True)
